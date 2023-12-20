fd7fa7d2f29fee4a14b34e4ec491d7a13a2a06c4
Mistaweng <emmanuelgyang.1822@gmail.com>
2023-12-20 21:53:34 +0100

    Added Controllers

---
+++ Enlab_Voting_Backend/Controllers/ContestantController.cs
@@ -0,0 +1,54 @@
+﻿using Enlab_Voting_Backend.Model;
+using Enlab_Voting_Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enlab_Voting_Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+
+	public class ContestantController : ControllerBase
+	{
+		private readonly IContestantInterfaceRepo _contestantRepo;
+
+		public ContestantController(IContestantInterfaceRepo contestantRepo)
+		{
+			_contestantRepo = contestantRepo;
+		}
+
+		[HttpPost("vote/{voterId}/{contestantId}")]
+		public async Task<IActionResult> VoteForCandidate(string voterId, string contestantId)
+		{
+			var result = await _contestantRepo.VoteForCandidateAsync(voterId, contestantId);
+
+			switch (result)
+			{
+				case 1:
+					return Ok("Vote successful");
+				case -1:
+					return BadRequest("Voter has already voted for the candidate");
+				default:
+					return BadRequest("Vote failed");
+			}
+		}
+
+		[HttpGet("vote-count/{contestantId}")]
+		public async Task<IActionResult> GetVoteCount(string contestantId)
+		{
+			var voteCount = await _contestantRepo.GetVoteCountAsync(contestantId);
+
+			return Ok(voteCount);
+		}
+
+		// Uncomment the following if you implement GetVoteHistoryAsync in IContestantInterfaceRepo
+		// [HttpGet("vote-history/{contestantId}")]
+		// public async Task<IActionResult> GetVoteHistory(string contestantId)
+		// {
+		//     var voteHistory = await _contestantRepo.GetVoteHistoryAsync(contestantId);
+		//     return Ok(voteHistory);
+		// }
+	}
+
+
+}
---
+++ Enlab_Voting_Backend/Controllers/FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using Enlab_Voting_Backend.Dtos;
+using Enlab_Voting_Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enlab_Voting_Backend.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FileUploadController : ControllerBase
+	{
+
+		private readonly IFileUploadRepository _fileUploadRepository;
+
+		public FileUploadController(IFileUploadRepository fileUploadRepository)
+		{
+			_fileUploadRepository = fileUploadRepository;
+		}
+		[HttpPost("image/FIle-upload")]
+		public async Task<IActionResult> UploadImageOrFile(IFormFile file)
+		{
+			var uploadedImage = await _fileUploadRepository.UploadImageToCloudinaryAndSave(file);
+
+			if (uploadedImage == null)
+			{
+				return NotFound(ApiResponse.Failed(uploadedImage));
+			}
+			return Ok(ApiResponse.Success(uploadedImage));
+		}
+	}
+}
